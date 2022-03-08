19ef13f2b1430c93d52c23b9410ddb1175b52983
danielmg98 <danielmarting98@gmail.com>
2022-03-08 19:04:15 +0100

    Eliminados nombres, comentarios

--- Mavir/program.py
+++ Mavir/program.py
@@ -9,6 +9,16 @@ if os.path.isfile('./Texto_nuevo/mavir02_new2.txt'):
 if os.path.isfile('./Texto_nuevo/mavir02_new3.txt'):
 	os.remove('./Texto_nuevo/mavir02_new3.txt')
 
+if os.path.isfile('./Texto_nuevo/mavir02_new4.txt'):
+	os.remove('./Texto_nuevo/mavir02_new4.txt')
+
+if os.path.isfile('./Texto_nuevo/mavir02_new5.txt'):
+	os.remove('./Texto_nuevo/mavir02_new5.txt')
+
+if os.path.isfile('./Texto_nuevo/mavir02_new6.txt'):
+	os.remove('./Texto_nuevo/mavir02_new6.txt')
+
+
 def eliminar_especiales(original, nuevo):
 	while 1:
 		char = original.read(1)
@@ -79,6 +89,56 @@ def eliminar_xxx(original, nuevo):
                 else:
                         nuevo.write(char)
 
+def eliminar_amp(original, nuevo):
+	while 1:
+		char = original.read(1)
+		if not char:
+			break
+		if char =='&':
+			char2 = original.read(1)
+			if char2 == ' ':
+				pass
+			elif char2 == 'a' or 'e' or 'u' or 'm':
+				char3 = original.read(1)
+				if char3:
+					pass
+			else:
+				pass
+		else :
+			nuevo.write(char)
+			
+		
+
+def eliminar_comentarios(original, nuevo):
+	while 1:
+		char = original.read(1)
+		if not char:
+			break
+		if char =='{':
+			char2 = original.read(1)
+			while char2 != '}':
+				char2 = original.read(1)
+			pass
+		else:
+			nuevo.write(char)	
+
+
+def eliminar_nombres(original, nuevo):
+	while 1:
+		line = original.readline()
+		if not line:
+			break
+		name = line[0:2]
+		if name == 'ANS' or 'LRO' or 'JSL' or 'ENR' or 'IRA' or 'ANT' or 'ISI':
+			newline = line[3:]
+			nuevo.write(newline)
+			pass
+		else:
+			nuevo.write(line)
+
+
+
+
 mavir = open('./Texto_original/mavir02.txt','r')
 mavir1 = open('./Texto_nuevo/mavir02_new.txt','w')
 
@@ -102,3 +162,33 @@ eliminar_xxx(mavir,mavir1)
 
 mavir.close()
 mavir1.close()
+
+
+mavir = open('./Texto_nuevo/mavir02_new3.txt','r')
+mavir1 = open('./Texto_nuevo/mavir02_new4.txt','w')
+
+eliminar_amp(mavir,mavir1)
+
+mavir.close()
+mavir1.close()
+
+mavir = open('./Texto_nuevo/mavir02_new4.txt','r')
+mavir1 = open('./Texto_nuevo/mavir02_new5.txt','w')
+
+eliminar_comentarios(mavir,mavir1)
+
+mavir.close()
+mavir1.close()
+
+
+mavir = open('./Texto_nuevo/mavir02_new5.txt','r')
+mavir1 = open('./Texto_nuevo/mavir02_new6.txt','w')
+
+eliminar_nombres(mavir,mavir1)
+
+mavir.close()
+mavir1.close()
+
+
+
+
